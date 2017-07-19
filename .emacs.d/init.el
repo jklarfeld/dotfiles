3e216899b310db66279b2a91e3a47c309781f24e
Jeffrey Klarfeld <jklarfeld@me.com>
2017-07-18 22:58:44 -0700

    Remove .emacs file to prevent it from overriding .emacs.d folder.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+; Stop leaving emacs-eggs® all over the file system.
+(setq make-backup-files nil)
+
+; Load some dependencies
 (add-to-list 'load-path "~/.dot-deps/dockerfile-mode/")
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
