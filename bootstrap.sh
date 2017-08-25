2b48fd377f59b1dbbce6a73730e9f899398dbd54
Jeffrey Klarfeld <jklarfeld@me.com>
2017-08-24 23:46:06 -0700

    Installs Spectacle shortcuts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "*.sh" --exclude "*.terminal" --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~ 
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "*.sh" --exclude "*.terminal" --exclude "*.json" --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~ 
+	SHORTCUTPATH="${HOME}/Library/Application Support/Spectacle"
+	mkdir -p "${SHORTCUTPATH}" 
+	cp ./Spectacle-Shortcuts.json "${SHORTCUTPATH}"/Shortcuts.json 
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
