f9dc2f8a5b92be48dac8511ff8bd8a16aed4d954
Jeffrey Klarfeld <jklarfeld@me.com>
2017-10-07 22:17:54 -0700

    some emacs tweaks, xcode ramdisk now created apfs ramdisks.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,20 @@
 ; Stop leaving emacs-eggs® all over the file system.
 (setq make-backup-files nil)
 
+; Melpa
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("MELPA Stable" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 ; Load some dependencies
 (add-to-list 'load-path "~/.dot-deps/dockerfile-mode/")
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+(use-package flycheck
+	     :ensure t
+	     :init (global-flycheck-mode))
+
+(eval-after-load 'flycheck '(flycheck-swift-setup))
