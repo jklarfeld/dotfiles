5209d8a3b25c9d830d81a21b74e6740fd6722f1f
Jeffrey Klarfeld <jklarfeld@me.com>
2021-07-07 23:02:39 -0700

    Switched to modern Brew installer.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 function installBrew() 
 {
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 installBrew
